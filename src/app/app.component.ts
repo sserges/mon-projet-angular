470512e9caa80fd8c32bb1d614d56ce4e5773f91
sserges <agounkeserge@gmail.com>
2019-08-08 18:54:38 +0000

    Edit data in real time with Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(() => {
+        resolve(date);
+      }, 2000);
+    }
+  );
+
   appareils = [
     {
       name: 'Machine à laver',
