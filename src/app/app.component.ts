f65b7474aed7dc5baaba81485bfe3a863872bc84
sserges <agounkeserge@gmail.com>
2019-09-03 16:30:05 +0000

    Manage navigation with Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AppareilService } from './services/appareil.service';
 
@@ -7,37 +7,8 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  isAuth = false;
+export class AppComponent {
+  constructor() {
 
-  lastUpdate = new Promise(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(() => {
-        resolve(date);
-      }, 2000);
-    }
-  );
-
-  appareils: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(() => {
-      this.isAuth = true;
-    }, 4000);
-  }
-
-  ngOnInit() {
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre() {
-    if (confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    }
   }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,18 +1,28 @@
 export class AppareilService {
     appareils = [
         {
+            id: 1,
             name: 'Machine à laver',
             status: 'éteint'
         },
         {
+            id: 2,
             name: 'Télévision',
             status: 'éteint'
         },
         {
+            id: 3,
             name: 'Ordinateur',
             status: 'éteint'
         }
     ];
+
+    getAppareilById(id: number) {
+        const appareil = this.appareils.find((appareilObject) => {
+            return appareilObject.id === id;
+        });
+        return appareil;
+    }
 
     switchOnAll() {
         for (const appareil of this.appareils) {
