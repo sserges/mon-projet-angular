335bd3411ad84b70e84c6b1d6edeff17de43b62d
sserges <agounkeserge@gmail.com>
2019-09-12 17:15:53 +0000

    Observe the data with RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-
-import { AppareilService } from './services/appareil.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  secondes: number;
+  counterSubscription: Subscription;
+
   constructor() {
 
+  }
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      (error: any) => {
+        console.log('Une erreur a été rencontrée !');
+      },
+      () => {
+        console.log('Observable complétée');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,5 +1,8 @@
+import { Subject } from 'rxjs/Subject';
+
 export class AppareilService {
-    appareils = [
+    appareilSubject = new Subject<any[]>();
+    private appareils = [
         {
             id: 1,
             name: 'Machine à laver',
@@ -17,6 +20,10 @@ export class AppareilService {
         }
     ];
 
+    emitAppareilSubject() {
+        this.appareilSubject.next(this.appareils.slice());
+    }
+
     getAppareilById(id: number) {
         const appareil = this.appareils.find((appareilObject) => {
             return appareilObject.id === id;
@@ -28,19 +35,23 @@ export class AppareilService {
         for (const appareil of this.appareils) {
             appareil.status = 'allumé';
         }
+        this.emitAppareilSubject();
     }
 
     switchOffAll() {
         for (const appareil of this.appareils) {
             appareil.status = 'éteint';
         }
+        this.emitAppareilSubject();
     }
 
     switchOnOne(index: number) {
         this.appareils[index].status = 'allumé';
+        this.emitAppareilSubject();
     }
 
     switchOffOne(index: number) {
         this.appareils[index].status = 'éteint';
+        this.emitAppareilSubject();
     }
 }
